5783385fde34e8b658e31d5ff4ac632e5eeab705
Bikendra Thapaliya <thapaliyabikendra@gmail.com>
2019-02-04 16:18:56 +0530

    main module

--- main.py
+++ main.py
@@ -1,42 +1,152 @@
+from picamera import PiCamera
+from subprocess import Popen, PIPE
+import threading
+from time import sleep
+import os, fcntl
 import cv2
-from darkflow.net.build import TFNet
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+import RPi.GPIO as gpio
+import time
+import numpy as np
+import random
 
-img = cv2.imread('image.jpg')
-print(img.shape)
-resized = cv2.resize(img, (256, 256))
-print(resized.shape)
+moves = 3
+learningRate = 0.9
+epsilon = 1.0
+epsilon_min = 0.01
+epsilon_decay = 0.995
+epochs = 10
+memory = []
+max_memory = 500
+iframe = 0
+camera = PiCamera()
+camera.resolution = (608, 608)
+camera.capture('frame.jpg')
+sleep(0.1)
 
-options = {"model": "cfg/tiny.cfg", 
-           "load": "tiny.weights", 
-           "threshold": 0.1}
-tfnet = TFNet(options)
+yolo_proc = Popen(["./darknet",
+                   "detect",
+                   "./cfg/yolov3-tiny.cfg",
+                   "./yolov3-tiny.weights",
+                   "-thresh","0.1"],
+                   stdin = PIPE, stdout = PIPE)
 
-original_img = cv2.imread("image.jpg")
-#original_img = cv2.resize(original_img, (224, 224))
-original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+fcntl.fcntl(yolo_proc.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
 
-results = tfnet.return_predict(original_img)
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(608, 608, 3), activation = 'relu'))
+model.add(Conv2D(64, (3, 3), activation = 'relu'))
+model.add(Conv2D(128, (3, 3), activation = 'relu'))
+model.add(Flatten())
+model.add(Dense(512, activation = 'relu'))
+model.add(Dense(3, activation = 'softmax'))
+model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
 
-def boxing(original_img, predictions):
-    newImage = np.copy(original_img)
+def init():
+	gpio.setmode(gpio.BCM)
+	gpio.setup(17, gpio.OUT)
+	gpio.setup(22, gpio.OUT)
+	gpio.setup(23, gpio.OUT)
+	gpio.setup(24, gpio.OUT)
 
-    for result in predictions:
-        top_x = result['topleft']['x']
-        top_y = result['topleft']['y']
+def forward(tf):
+	init()
+	gpio.output(17, True)
+	gpio.output(22, False)
+	gpio.output(23, True)
+	gpio.output(24, False)
+	time.sleep(tf)
+	gpio.cleanup()
 
-        btm_x = result['bottomright']['x']
-        btm_y = result['bottomright']['y']
+def reverse(tf):
+	init()
+	gpio.output(17, False)
+	gpio.output(22, True)
+	gpio.output(23, False)
+	gpio.output(24, True)
+	time.sleep(tf)
+	gpio.cleanup()
+def left(tf):
+	init()
+	gpio.output(17, False)
+	gpio.output(22, True)
+	gpio.output(23, True)
+	gpio.output(24, False)
+	time.sleep(tf)
+	gpio.cleanup()
+def right(tf):
+	init()
+	gpio.output(17, True)
+	gpio.output(22, False)
+	gpio.output(23, False)
+	gpio.output(24, True)
+	time.sleep(tf)
+	gpio.cleanup()
 
-        confidence = result['confidence']
-        label = result['label'] + " " + str(round(confidence, 3))
+def getFrames():
+	errors = True
+	im = []
+	try:
+		stdout = yolo_proc.stdout.read()
+		if 'Enter Image Path' in stdout:
+			try:
+				im = cv2.imread('predictions.png')
+				cv2.imshow('yolov3-tiny', im)
+				key = cv2.waitKey(1)
+				print(im.shape)
+			except Exception:
+				pass
+			camera.capture('frame.jpg')
+			yolo_proc.stdin.write('frame.jpg\n')
+		if len(stdout.strip())>0:
+			print('get %s' % stdout)
+			errors = False
+	except Exception:
+		pass
+	return im, errors
 
-        if confidence > 0.3:
-            newImage = cv2.rectangle(newImage, (top_x, top_y), (btm_x, btm_y), (255,0,0), 3)
-            newImage = cv2.putText(newImage, label, (top_x, top_y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL , 0.8, (0, 230, 0), 1, cv2.LINE_AA)
-            
-    return newImage
-
-import matplotlib.pyplot as plt
-
-_, ax = plt.subplots(figsize=(20, 10))
-ax.imshow(boxing(original_img, results))
+for i in range(epochs):
+	time.sleep(5)
+	game_over = False
+	input_img, errors = getFrames()
+	errors = False
+	reward = 0
+	while game_over==False:
+		if np.random.rand() <= epsilon:
+			action = np.random.randint(0, moves, size=1)[0]
+		else:
+			output = model.predict(input_img)
+			action = np.argmax(output[0])
+		if int(action) == 0:
+			forward(4)
+		elif int(action) == 1:
+			right(4)
+		else:
+			left(4)
+		input_next_img, errors = getFrames()
+		if errors == False:
+			reward = reward + 1
+		else:
+			game_over = True
+		if len(memory) >= max_memory:
+			del memory[0]
+		memory.append((input_img, action, reward, input_next_img, game_over))
+		input_img = input_next_img
+		if game_over:
+			print("Game: {}/{}, Total Reward: {}".format(i, epochs, reward))
+	if len(memory) > 32:
+		batch_size = 32
+	else:
+		batch_size = len(memory)
+	batch = random.sample(memory, batch_size)
+	for input_img, action, reward, input_next_img, game_over in batch:
+		target_reward = reward
+		if game_over == False:
+			target_reward = reward + learningRate * \
+			np.amax(model.predict(input_next_img)[0])
+		desired_target = model.predict(input_img)
+		desired_target[0][action] = target_reward
+		model.fit(input_img, desired_target, epochs=1, verbose=0)
+	if epsilon > epsilon_min:
+		epsilon *= epsilon_decay
