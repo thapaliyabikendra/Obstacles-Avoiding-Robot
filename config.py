253b7086dab943595e42bcd050b40b27c8481796
Bikendra Thapaliya <thapaliyabikendra@gmail.com>
2019-05-01 20:32:36 +0530

    a

--- config.py
+++ config.py
@@ -1,4 +1,32 @@
-WIDTH, HEIGHT = 240,320
+#image
+WIDTH = 320
+HEIGHT = 240
 CHANNEL = 1
+
+#servo
 SERVO = 16
+
+
+#preview
+CP_WIDTH = 608
+CP_HEIGHT = 608
+
+
+#motor driver L298N
+FREQUENCY = 1000
+DUTY_CYCLE = 90
+ENA = 20
+ENB = 21
+IN1 = 17
+IN2 = 22
+IN3 = 23
+IN4 = 24
+FB_TIME = 0.8
+LR_TIME = 0.8
+
+#ultrasonic
+TRIGGER = 14
+ECHO = 15
+THRESHOLD = 0
+MINIMUM_DISTANCE = 20
 
